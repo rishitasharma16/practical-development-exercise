0090763d927d4125238b53111257310ec479ce38
rishita sharma <mailrishitasharma@gmail.com>
2025-01-12 16:34:10 +0530

    netlify link updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/practical-development-exercise.git/",
+  
   plugins: [react()],
 })
